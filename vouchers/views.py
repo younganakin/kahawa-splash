34c033f3f8b3d50722c88e325d6a9e0e9e235a3c
younganakin <joenjuguna53@gmail.com>
2018-10-09 17:04:21 +0300

    Made changes to index.html.

--- vouchers/views.py
+++ vouchers/views.py
@@ -31,15 +31,15 @@ def download_vouchers(request):
 
 def generate_codes(request):
     voucher_url = 'http://178.62.86.105/gen_codes'
-    voucher_params_1 = {"numbers": 4}
-    voucher_params_2 = {"numbers": 4}
-    voucher_params_3 = {"numbers": 4}
-    voucher_params_4 = {"numbers": 4}
-    voucher_params_5 = {"numbers": 4}
-    voucher_params_6 = {"numbers": 4}
-    voucher_params_7 = {"numbers": 4}
-    voucher_params_8 = {"numbers": 4}
-    voucher_params_9 = {"numbers": 4}
+    voucher_params_1 = {"numbers": 3}
+    voucher_params_2 = {"numbers": 3}
+    voucher_params_3 = {"numbers": 3}
+    voucher_params_4 = {"numbers": 3}
+    voucher_params_5 = {"numbers": 3}
+    voucher_params_6 = {"numbers": 3}
+    voucher_params_7 = {"numbers": 3}
+    voucher_params_8 = {"numbers": 3}
+    voucher_params_9 = {"numbers": 3}
     r_1 = requests.get(voucher_url, params=voucher_params_1)
     r_2 = requests.get(voucher_url, params=voucher_params_2)
     r_3 = requests.get(voucher_url, params=voucher_params_3)
@@ -48,7 +48,6 @@ def generate_codes(request):
     r_6 = requests.get(voucher_url, params=voucher_params_6)
     r_7 = requests.get(voucher_url, params=voucher_params_7)
     r_8 = requests.get(voucher_url, params=voucher_params_8)
-    r_9 = requests.get(voucher_url, params=voucher_params_9)
     voucher_codes_1 = json.loads(r_1.text)
     voucher_codes_2 = json.loads(r_2.text)
     voucher_codes_3 = json.loads(r_3.text)
@@ -57,7 +56,6 @@ def generate_codes(request):
     voucher_codes_6 = json.loads(r_6.text)
     voucher_codes_7 = json.loads(r_7.text)
     voucher_codes_8 = json.loads(r_8.text)
-    voucher_codes_9 = json.loads(r_9.text)
 
     context = {
         'voucher_codes_1': voucher_codes_1,
@@ -68,7 +66,6 @@ def generate_codes(request):
         'voucher_codes_6': voucher_codes_6,
         'voucher_codes_7': voucher_codes_7,
         'voucher_codes_8': voucher_codes_8,
-        'voucher_codes_9': voucher_codes_9,
     }
 
     return render(request, 'vouchers/vouchers.html', context)
